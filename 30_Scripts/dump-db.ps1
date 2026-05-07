af989f6542858e5375740650370771281ed26d6e
ento <105713785+codingThome@users.noreply.github.com>
2026-05-07 10:47:11 +0200

    Arbeitsanweisung: 2026-05-07 10:47:11

--- 30_Scripts/dump-db.ps1
+++ 30_Scripts/dump-db.ps1
@@ -1,17 +1,17 @@
 # Konfiguration
-$DB_NAME = "dbfriends"
+$DB_NAME = "legoshop"
 $DB_USER = "root"
 $DB_PASSWORD = ""
 
 # Pfad zum Dump-Tool
-$MYSQLDUMP = "F:\_schule2026\_wids3bk_2\xampp\mysql\bin\mysqldump.exe"
+$MYSQLDUMP = "F:\_schule2026\_wids3dk\xampp\mysql\bin\mysqldump.exe"
 
 # Timestamp
 $timestamp = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
 
 
 # Output-Datei (im Git-Repo!)
-$output = "F:\_schule2026\_wids3bk_2\dump.sql"
+$output = "F:\_schule2026\_abschlussProjekte\AngabeAbschlussProjekt_3dk_2026\dump.sql"
 
 Write-Host "Starte Datenbank-Dump..."
 
@@ -22,7 +22,11 @@ if (!(Test-Path $MYSQLDUMP)) {
 }
 
 # Dump ausführen
-& $MYSQLDUMP -u $DB_USER --databases $DB_NAME --routines --triggers --events | Out-File -Encoding utf8 $output
+#& $MYSQLDUMP -u $DB_USER --default-character-set=utf8mb4 --databases $DB_NAME --routines --triggers --events | Out-File -Encoding utf8 $output
+
+$cmd = "`"$MYSQLDUMP`" -u $DB_USER --databases $DB_NAME --routines --triggers --events > `"$output`""
+
+cmd.exe /c $cmd
 
 # Erfolg prüfen
 if ($LASTEXITCODE -eq 0) {
